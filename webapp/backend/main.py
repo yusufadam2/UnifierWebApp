72d096872092949829a2b890f8f549b21cf0c910
w79958ml <mikolaj.lenczewski@student.manchester.ac.uk>
2021-02-14 00:24:01 +0000

    Made crypto more consistent, improved sqldb bootstrap, added basic REST API
    The crypto module now has more consistent parameter ordering in its API.
    The sqldb module bootstrap method now adds data to the created db.
      - A default profile test picture was added
    Basic register, login, and logout methods added to the backend REST API.

--- webapp/backend/main.py
+++ webapp/backend/main.py
@@ -1,11 +1,20 @@
+import crypto
+import sqldb
+
 from flask import json, request, Flask
 
 
 # TODO(mikolaj): remove static_* parameters for production
 app = Flask(__name__, static_url_path='', static_folder='../static')
 
-app.secret_key = 'W6-CCOMP10120'
+app.secret_key = 'W6-COMP10120'
 app.token_key = 'csrf_token'
+
+
+conn = sqldb.try_open_conn()
+assert conn is not None  # we can't open a connection to the database
+
+cur = conn.cursor()  # this cursor allows us to execute sql queries
 
 
 @app.route('/')
@@ -16,10 +25,10 @@ def main():
 @app.route('/test/')
 def test():
     data = ['foo', 'bar', 42]
-    return app.response_class(
-            response=json.dumps(data), status=200, mimetype='application/json')
+    return app.response_class(response=json.dumps(data), status=200, mimetype='application/json')
 
 
+# TODO(mikolaj): implement csrf protection
 @app.route('/register/', methods=['POST'])
 def register():
     email = request.args['email']
@@ -29,26 +38,31 @@ def register():
     return app.response_class(status=200)
 
 
+# TODO(mikolaj): implement csrf protection
 @app.route('/login/', methods=['POST'])
 def login():
     username = request.args['user']
     password = request.args['pass']
 
-    print(f'User: {username}, Pass: {password}')
+    query = 'SELECT userId, hash, salt FROM UserAuth WHERE username LIKE ?'
+    parameters = (username,)
+    matching_users = sqldb.do_sql(cur, query, parameters)
+
+    for uid, user_hash, user_salt in matching_users:
+        if crypto.verify_secret(password, user_hash, user_salt):
+            print(f'[user-{uid}] Logged in!')
+
+            return app.response_class(status=200)
+
+    return app.response_class(status=401)
+
+
+# TODO(mikolaj): implement csrf protection
+@app.route('/logout/', methods=['POST'])
+def logout():
+    uid = request.args['uid']
+
+    print(f'[user-{uid}] Logged out!')
 
     return app.response_class(status=200)
-
-
-@app.route('/userauth/')
-def userauth():
-    data = ['foo', 'bar']
-    return app.response_class(
-            response=json.dumps(data), status=200, mimetype='application/json')
-
-
-@app.route('/users/')
-def users():
-    data = ['foo', 'bar']
-    return app.response_class(
-            response=json.dumps(data), status=200, mimetype='application/json')
 
--- webapp/backend/sqldb.py
+++ webapp/backend/sqldb.py
@@ -2,11 +2,17 @@
 This module provides a helper function for executing sql queries.
 """
 
-__all__ = ['DB', 'NAME_LEN', 'DESC_LEN', 'HASH_LEN', 'PATH_LEN', 'do_sql']
+__all__ = [
+        'DB', 'NAME_LEN', 'DESC_LEN', 'HASH_LEN', 'SALT_LEN', 'PATH_LEN',
+        'close_conn', 'do_sql', 'try_open_conn'
+]
 
 import enum
 import sqlite3 as sql
 
+import crypto
+
+from datetime import datetime
 from typing import List, Optional, Tuple
 
 ## TODO:
@@ -19,8 +25,23 @@ from typing import List, Optional, Tuple
 DB = 'w6-comp10120.db'
 NAME_LEN = 100
 DESC_LEN = 4096
-HASH_LEN = 512
+HASH_LEN = crypto.CRYPTO_HASH_LEN
+SALT_LEN = crypto.CRYPTO_SALT_LEN
 PATH_LEN = 256
+
+
+def close_conn(conn: sql.Connection):
+    conn.commit()
+    conn.close()
+
+
+def try_open_conn() -> Optional[sql.Connection]:
+    try:
+        conn = sql.connect(DB)
+        return conn
+
+    except sql.OperationalError as err:
+        return None
 
 
 def do_sql(cur: sql.Cursor, query: str, parameters: Tuple = None) -> Optional:
@@ -44,6 +65,7 @@ def bootstrap():
     conn = sql.connect(DB)
     cur = conn.cursor()
 
+    # create tables
     do_sql(cur, '''DROP TABLE UserAuth''')
     do_sql(cur, '''CREATE TABLE UserAuth(
         id INTEGER UNIQUE NOT NULL PRIMARY KEY,
@@ -51,7 +73,6 @@ def bootstrap():
         email VARCHAR(100) UNIQUE NOT NULL,
         hash VARCHAR(512) UNIQUE NOT NULL,
         salt VARCHAR(512) UNIQUE NOT NULL,
-        iter INTEGER NOT NULL,
         userId INTEGER UNIQUE NOT NULL,
         FOREIGN KEY(userId) REFERENCES Users(id));''')
 
@@ -60,7 +81,7 @@ def bootstrap():
         id INTEGER UNIQUE NOT NULL PRIMARY KEY,
         name VARCHAR(100) NOT NULL,
         dob DATE NOT NULL,
-        gender VARCHAR(100) NOT NULL,
+        gender VARCHAR(100),
         bio VARCHAR(4096),
         pictureId INTEGER NOT NULL,
         FOREIGN KEY(pictureId) REFERENCES UserPictures(id));''')
@@ -95,6 +116,43 @@ def bootstrap():
         conversationId INTEGER NOT NULL,
         FOREIGN KEY(userId) REFERENCES Users(id),
         FOREIGN KEY(conversationId) REFERENCES Conversations(id));''')
+
+    # insert data
+    default_picture_fpath = 'default_profile_picture.png'
+    with open(default_picture_fpath, 'rb') as default_picture_file:
+        default_picture_blob = default_picture_file.read()
+
+        CREATE_USER_PICTURE_QUERY = 'INSERT INTO UserPictures (data) VALUES (?);'
+        do_sql(cur, CREATE_USER_PICTURE_QUERY, (default_picture_blob,))
+
+    CREATE_USER_QUERY = 'INSERT INTO Users (name, dob, pictureId) VALUES (?,?,?);'
+    do_sql(cur, CREATE_USER_QUERY, ('John Doe', datetime.now(), 1))
+    do_sql(cur, CREATE_USER_QUERY, ('Jane Doe', datetime.now(), 1))
+    do_sql(cur, CREATE_USER_QUERY, ('Foo Bar', datetime.now(), 1))
+
+    CREATE_USER_AUTH_QUERY = 'INSERT INTO UserAuth (username, email, hash, salt, userId) VALUES (?,?,?,?,?);'
+    do_sql(cur, CREATE_USER_AUTH_QUERY, ('jdoe1991', 'john.doe@gmail.com', *crypto.hash_secret('password'), 1))
+    do_sql(cur, CREATE_USER_AUTH_QUERY, ('jane_doe', 'jane.doe@gmail.com', *crypto.hash_secret('ABetterPassword!'), 2))
+    do_sql(cur, CREATE_USER_AUTH_QUERY, ('foobar', 'foobar@gmail.com', *crypto.hash_secret('foobar?'), 3))
+
+    CREATE_INTEREST_QUERY = 'INSERT INTO Interests (name) VALUES (?);'
+    interests = ['sport', 'science', 'food', 'music', 'reading', 'hiking']
+    for i in interests:
+        do_sql(cur, CREATE_INTEREST_QUERY, (i,))
+
+    JOIN_USER_INTEREST_QUERY = 'INSERT INTO UsersInterestsJoin (userId, interestId) VALUES (?,?);'
+    johns_interests = [1, 3, 4]
+    for i in johns_interests:
+        do_sql(cur, JOIN_USER_INTEREST_QUERY, (1, i))
+
+    janes_interests = [2, 5, 6]
+    for i in janes_interests:
+        do_sql(cur, JOIN_USER_INTEREST_QUERY, (2, i))
+
+    foobars_interests = [1, 2, 3, 4, 5, 6]
+    for i in foobars_interests:
+        do_sql(cur, JOIN_USER_INTEREST_QUERY, (3, i))
+
 
     conn.commit()
     conn.close()
